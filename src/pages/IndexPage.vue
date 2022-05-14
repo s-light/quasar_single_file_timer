57d66b04edd6a9fe415b200638f010d67af244d1
s-light <git@s-light.eu>
2022-05-14 17:14:12 +0200

    tweaks

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,12 +3,12 @@
         Countdown
         <q-circular-progress
             :value="value"
-            size="90vh"
+            size="8 0vh"
             :thickness="1"
             color="orange"
             track-color="grey-3"
             center-color="grey-8"
-            class="q-ma-md"
+
             show-value
             font-size="20vh"
         >
@@ -20,10 +20,30 @@
 
 <script>
 import { defineComponent } from 'vue'
+import { useQuasar } from 'quasar'
 
 export default defineComponent({
     name: 'IndexPage',
     setup () {
+        const $q = useQuasar()
+
+        // https://quasar.dev/quasar-plugins/addressbar-color
+        $q.addressbarColor.set('#a2e3fa')
+
+        // dark mode
+        // get status
+          console.log($q.dark.isActive) // true, false
+
+          // get configured status
+          console.log($q.dark.mode) // "auto", true, false
+
+          // set status
+          $q.dark.set(true) // or false or "auto"
+
+          // toggle
+          // $q.dark.toggle()
+          $q.notify('Message')
+
         return {
             value: 71,
         }
